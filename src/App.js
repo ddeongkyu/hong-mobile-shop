1bfb4d24ef8fec63217b9ce0738d0a061895d029
ddeongkyu <tjdrb4230@naver.com>
2022-06-04 20:35:52 +0900

    Creating Basic Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HeadIcon from "./components/HeadIcon";
+import HeadText from "./components/HeadText";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HeadIcon />
+      <HeadText />
+      <Main />
+    </>
   );
 }
 
---
+++ src/components/Main.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { AiOutlineArrowRight } from "react-icons/ai";
+function Main() {
+  return (
+    <div>
+      <div className="MainBlock">
+        <div className="classicBox">
+          <div className="classicBoxLeft">
+            The Classic
+            <br />
+            <span className="classicApperance">Classic apperance</span>
+            <br />
+            <span className="classicApperance">With a splash of color</span>
+            <div>
+              <AiOutlineArrowRight className="AiOutlineArrowRight" />
+            </div>
+          </div>
+          <div className="classicBoxRight">
+            <img
+              className="classicBoxRight"
+              src={"/img/fabricChair.jpg"}
+              alt="fabricChair"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="MainTextBlock">
+        <div className="MainSellOrPicks">Best Sellers</div>
+      </div>
+      <div className="productWrappingBox">
+        <span className="productBox">
+          <img
+            className="productPictureStyle"
+            src={"/img/blackChair.jpg"}
+            alt="카떼라떼"
+          />
+        </span>
+        <span className="productBox">
+          <img
+            className="productPictureStyle"
+            src={"/img/fabricChair.jpg"}
+            alt="카떼라떼"
+          />
+        </span>
+        <span className="productBox">
+          <img
+            className="productPictureStyle"
+            src={"/img/greyAndWoodChair.jpg"}
+            alt="카떼라떼"
+          />
+        </span>
+        <span className="productBox">
+          <img
+            className="productPictureStyle"
+            src={"/img/whiteChairWithPink풍선.jpg"}
+            alt="카떼라떼"
+          />
+        </span>
+      </div>
+
+      <div className="MainTextBlock">
+        <div className="MainSellOrPicks">Hand-picks</div>
+        <div className="productWrappingBox">
+          <div className="productBox">
+            <img
+              className="productPictureStyle"
+              src={"/img/pinkChair.jpg"}
+              alt="카떼라떼"
+            />
+          </div>
+          <div className="productBox">
+            <img
+              className="productPictureStyle"
+              src={"/img/whiteChair.jpg"}
+              alt="카떼라떼"
+            />
+          </div>
+          <div className="productBox">
+            <img
+              className="productPictureStyle"
+              src={"/img/royalStyleChair.jpg"}
+              alt="카떼라떼"
+            />
+          </div>
+          <div className="productBox">
+            <img
+              className="productPictureStyle"
+              src={"/img/brownChair.jpg"}
+              alt="카떼라떼"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
